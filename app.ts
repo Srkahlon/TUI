e5cff1f55a8eca694bc841e9b51352c5256be21a
Simran <simran.kahlon@graymatrix.com>
2021-04-01 22:44:37 +0530

    removed dotenv import statement

--- app.ts
+++ app.ts
@@ -1,8 +1,6 @@
-import dotenv from "dotenv";
 import express from "express";
 import bodyParser from 'body-parser';
 import {Routes} from './src/routes/apiRoutes';
-dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
